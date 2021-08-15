588d8b8801ddb4c3ad4335b3723fbb6908dc662c
Gino2B <gbaybay30@gmail.com>
2021-08-15 15:32:41 -0400

    Player screen styling, and more nav styling

--- client/src/screens/Players/Players.jsx
+++ client/src/screens/Players/Players.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getAllPlayers } from "../../services/players";
+import "./Players.css";
 
 const Players = () => {
   const [players, setPlayers] = useState([]);
@@ -14,26 +15,33 @@ const Players = () => {
   }, []);
 
   return (
-    <div>
+    <div className="players-screen-container">
+      <div className="players-screen-header">Players</div>
       <div className="player-header">
         <div>Picture</div>
         <div>Name</div>
         <div>Team</div>
         <div>Moments</div>
       </div>
-      {players.map((player, i) => (
-        <section
-          key={player.id}
-          className={i % 2 === 0 ? "evenClass" : "oddClass"}
-        >
-          <Link to={`/players/${player.id}`}>
-            <img src={`${player.image_url}`} alt={`${player.name} img`}></img>
-            <div>{player.name}</div>
-            <div>{player.team}</div>
-            <div>{player.moments?.length}</div>
-          </Link>
-        </section>
-      ))}
+      <div className="players-container">
+        {players.map((player, i) => (
+          <section key={player.id} className="player-container">
+            <Link
+              className={i % 2 === 0 ? "even-player" : "odd-player"}
+              to={`/players/${player.id}`}
+            >
+              <img
+                src={`${player.image_url}`}
+                alt={`${player.name} img`}
+                className="player-image"
+              ></img>
+              <div className="player-name">{player.name}</div>
+              <div className="player-team">{player.team}</div>
+              <div className="player-moments">{player.moments?.length}</div>
+            </Link>
+          </section>
+        ))}
+      </div>
     </div>
   );
 };
