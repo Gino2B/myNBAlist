a5e4c5588a1e993c3350bb66e75b9c74e531a8e9
Gino2B <gbaybay30@gmail.com>
2021-08-16 15:08:02 -0400

    fixing apiconfig

--- client/src/services/apiConfig.js
+++ client/src/services/apiConfig.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const base =
   process.env.NODE_ENV === "production"
-    ? "https://mynbashot-api.herokuapp.com/"
+    ? "https://mynbashot-api.herokuapp.com"
     : "http://localhost:3000";
 
 const api = axios.create({
